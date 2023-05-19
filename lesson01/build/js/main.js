81e451b7eb673053b5d3a67e7762372d4445718b
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-20 01:43:48 +0200

    ts basic data structures

--- lesson01/build/js/main.js
+++ lesson01/build/js/main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('zoki');
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+album = true;
+isLoading = true;
+const sum = (a, b) => {
+    return a + b;
+};
--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -1,1 +1,12 @@
-console.log('zoki');
+let myName: string = 'Dave'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any;
+
+
+album = true;
+isLoading = true
+
+const sum = (a: number,b: number) => {
+    return a + b
+}
