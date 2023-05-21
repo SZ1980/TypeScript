fbe2319e95d0d1a05283800d21c983106860873b
Zoran Simonovic <simonovic1980@gmail.com>
2023-05-21 17:37:20 +0200

    index signature

---
+++ lesson01/build/js/main07.js
@@ -0,0 +1,41 @@
+"use strict";
+//index signature
+// interface TransactionObj {
+//     // readonly [index: string]: number
+//     Pizza: number,
+//     Books: number,
+//     Job: number
+// }
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50,
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Books']);
+let prop = 'Pizza';
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+// console.log(student.test)
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
